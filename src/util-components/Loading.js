1ed235a526289cb37ae946ccb731b77a2bfe1944
Jonah Greenthal <jonah@naqt.com>
2023-08-26 21:52:17 -0500

    missing imports didn't seem to harm anything but seem like a good idea

--- src/util-components/Loading.js
+++ src/util-components/Loading.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import { CircularProgress } from '@mui/material'
 
 const Loading = () => (
--- src/util-components/Mailto.js
+++ src/util-components/Mailto.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 const address = process.env.REACT_APP_CONTACT_EMAIL
 
 const Mailto = () => (<a href={`mailto:${address}`}>{address}</a>)
