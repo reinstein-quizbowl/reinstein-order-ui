503db5e548a8dc636e5c84bcde38601f9e63b92d
Jonah Greenthal <jonah@jonahgreenthal.com>
2023-09-01 11:06:16 -0500

    add missing status; standardize sequence numbers

--- src/util/bookingUtil.js
+++ src/util/bookingUtil.js
@@ -1,10 +1,11 @@
-export const STATUS_UNSUBMITTED = { code: 'unsubmitted', label: 'Unsubmitted', sequence: 1 }
-export const STATUS_SUBMITTED = { code: 'submitted', label: 'Submitted', sequence: 2 }
-export const STATUS_APPROVED = { code: 'approved', label: 'Approved', sequence: 3 }
-export const STATUS_SHIPPED = { code: 'shipped', label: 'Shipped', sequence: 4 }
-export const STATUS_CANCELED = { code: 'canceled', label: 'Canceled', sequence: 100 }
-export const STATUS_REJECTED = { code: 'rejected', label: 'Rejected', sequence: 200 }
-export const ALL_STATUSES = [STATUS_UNSUBMITTED, STATUS_SUBMITTED, STATUS_APPROVED, STATUS_SHIPPED, STATUS_CANCELED, STATUS_REJECTED]
+export const STATUS_UNSUBMITTED = { code: 'unsubmitted', label: 'Unsubmitted', sequence: 100 }
+export const STATUS_SUBMITTED = { code: 'submitted', label: 'Submitted', sequence: 200 }
+export const STATUS_APPROVED = { code: 'approved', label: 'Approved', sequence: 300 }
+export const STATUS_SHIPPED = { code: 'shipped', label: 'Shipped', sequence: 400 }
+export const STATUS_ABANDONED = { code: 'abandoned', label: 'Abandoned', sequence: 1000 }
+export const STATUS_CANCELED = { code: 'canceled', label: 'Canceled', sequence: 1100 }
+export const STATUS_REJECTED = { code: 'rejected', label: 'Rejected', sequence: 1200 }
+export const ALL_STATUSES = [STATUS_UNSUBMITTED, STATUS_SUBMITTED, STATUS_APPROVED, STATUS_SHIPPED, STATUS_ABANDONED, STATUS_CANCELED, STATUS_REJECTED]
 
 export const getStatusData = (statusCode) => ALL_STATUSES.find(it => it.code === statusCode)
 
