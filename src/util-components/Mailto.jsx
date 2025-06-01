c37e701a7ccb2bca285e38df8c5c54025acad9b0
Jonah Greenthal <jonah@jonahgreenthal.com>
2025-05-31 20:24:46 -0500

    Migrate to Vite and remove create-react-app

--- src/util-components/Mailto.jsx
+++ src/util-components/Mailto.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const address = process.env.REACT_APP_CONTACT_EMAIL
+const address = import.meta.env.VITE_APP_CONTACT_EMAIL
 
 const Mailto = () => (<a href={`mailto:${address}`}>{address}</a>)
 
